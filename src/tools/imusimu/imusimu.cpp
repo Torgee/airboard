449bda9d30bb9d34d3cf6216e957e8d2b2b8b5c7
Torge Herber <torge.herber@gmail.com>
2014-03-14 15:37:54 +0700

    changed main function to be more like it should be, i.e. use classes for sensor abstraction, etc note, that there are still some curiosities, e.g. multiple calls to '.start()'

--- src/tools/imusimu/imusimu.cpp
+++ src/tools/imusimu/imusimu.cpp
@@ -1,20 +1,68 @@
 
-#include "simulation/IMUSimulator.h"
+#include <chrono>
+#include <random>
+#include <string>
+#include <thread>
+#include <memory>
+
+//#include "simulation/IMUSimulator.h"
+//#include "data_streaming/IPC_DataStreamer.h"
+
+#include "sensor/AbstractActiveSensor.h"
+#include "sensor/SensorModule.h"
+#include "sensor/SensorPoller.h"
+
+#include "sensor/simulation/PassiveSensorSimulator.h"
+
+#include "communication/AbstractPublisher.h"
+#include "communication/IPCPublisher.h"
+
 
 
 int main(int argc, char** argv)
 {
+	// *** parameter setup ***
+	// ToDo: replace by proper argument parsing OR by calling
+	// from python (and to argparse there)
 	
-	// ToDo: make program parameter for which kind of simulator to use!
-	IMUSimulator &imusim = SimpleIMUSimulator();
+	// * simulation parameter
 	
-	imusim.start();
+	// not supported currently
+	//int simulation_type = SIM_RANDOM;
 	
-	// ToDo: replace by proper waiting/interrupt
-	int a;
-	std::cin > a;
+	// random values in between [low,high), i.e.:
+	// val = low + [0.0,1.0) * (high-low)
+	double low = 0.0;
+	double high = 1.0;
+	double samplingRate = 200.; // in Hz
+		
 	
-	imusim.stop();
+	// create all those sensor things
+	
+	//sptr<RandomDataGenerator> randGen(rg_arams);
+	sptr<PassiveRandomSensorSimulator> sensorSimulator_random(low,high);
+	//sptr<IPCPublisher> publisher_IPC(ipc_arams);
+	sptr<IPCPublisher> publisher_IPC();
+	sptr<SensorPoller> sensorSimulatorPoller(
+			sensorSimulator_random,sp_params);
+	// ToDo: put back here, but currently not working concurrently
+	//sensorSimulatorPoller.start();
+		
+	// create alias for 'dependency injection' kinda
+	sptr<AbstractActiveSensor> sensor = simulatorPoller;
+	sptr<AbstractPublisher> publisher = publisher_IPC;
+	
+	SensorModule sensorModule(sensor,publisher)
+	
+	
+	// currently, 'start()' will trigger the polling once
+    std::chrono::milliseconds duration(int(1000./samplingRate));
+	while(true){
+		for(int i=0;i<numberOfChannels;++i){
+			sensorSimulatorPoller.start();
+		}
+		std::this_thread::sleep_for( duration );
+	}
 	
 	return 0;
 }
