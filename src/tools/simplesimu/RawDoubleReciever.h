e18fdbb3bd43846f594c19a3982abb0330cae633
Torge Herber <torge.herber@gmail.com>
2014-04-30 15:30:27 +0700

    added new source files for simple reciever class

---
+++ src/tools/simplesimu/RawDoubleReciever.h
@@ -0,0 +1,15 @@
+ 
+#ifndef RAWDOUBLERECIEVER_H
+#define RAWDOUBLERECIEVER_H
+
+class RawDoubleReciever {
+
+public:
+	RawDoubleReciever();
+	virtual ~RawDoubleReciever();
+	
+private:
+
+};
+
+#endif // #ifndef RAWDOUBLERECIEVER_H
