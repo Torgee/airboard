81a0770bcb1f763383bb9978fed14cfff8e194e1
Torge Herber <torge.herber@gmail.com>
2014-06-23 04:41:28 +0700

    first commit for python-based receiver (subscriber) does a basic timing for received messages, exit receiver with ctrl-c!

---
+++ src/tools/simplesimu/receiver.py
@@ -0,0 +1,49 @@
+
+
+import sys
+import timeit
+
+import zmq
+
+import double_pb2
+
+verbose = True if '-v' in sys.argv else False
+
+#  Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+print("Collecting updates from sensor")
+socket.connect("ipc:///tmp/sensor.ipc")
+
+# Subscribe to all messages
+socket.setsockopt_string(zmq.SUBSCRIBE, u"")
+
+i=0
+
+# warm up
+msg = socket.recv()
+
+
+last = 0
+start = timeit.default_timer()
+while(True):
+	try:
+		msg = socket.recv()
+		raw_d = double_pb2.RawDouble.FromString(msg)
+	except KeyboardInterrupt:
+		break
+	else:
+		if verbose:
+			print raw_d
+		i+=1
+		last = timeit.default_timer()
+		#print raw_d.value
+	
+time_spent = last-start
+if i == 0:
+	print 'no message received!'
+else:
+	print 'received {} messages in {}s\n{}s/msg\n{}hz'.format(
+			i,time_spent,time_spent/i,i/time_spent)
+
